f30a5e74a9ee100f1f4fec735a845187fe18a7cf
asharm29 <asharm29@unix2.csc.calpoly.edu>
2019-04-09 16:15:40 -0700

    location_tests

--- location_tests.py
+++ location_tests.py
@@ -4,10 +4,24 @@ from location import *
 class TestLab1(unittest.TestCase):
 
     def test_repr(self):
-        loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(repr(loc1),"Location('SLO', 35.3, -120.7)")
+        loc = Location("Paris", 48.9, 2.4)
+        self.assertEqual(repr(loc),"Location('Paris', 48.9, 2.4)")
+        loc4 = loc1
+        self.assertEqual(repr(loc4),"Location('SLO', 35.3, -120.7)")
     
-    # Add more tests!
+    def test_eq(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        self.assertFalse(loc1 == loc2)
+        loc3 = loc1
+        self.assertTrue(loc1 == loc3) 
+        loc4 = Location("SLO", 35.30000001, -120.6999999)
+        self.assertTrue(loc1 == loc4)
+        loc3 = Location("Paris", 48.901, 2.4001)
+        self.assertFalse(loc1 == loc4)
+        
 
 if __name__ == "__main__":
         unittest.main()
