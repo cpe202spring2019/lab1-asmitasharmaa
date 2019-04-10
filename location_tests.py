6bcd4cc56502ac32fe13fa6d8d1b3fae3ff0d94a
asharm29 <asharm29@csc303x14.ad.calpoly.edu>
2019-04-10 12:32:16 -0700

    comments

--- location_tests.py
+++ location_tests.py
@@ -2,7 +2,8 @@ import unittest
 from location import *
 
 class TestLab1(unittest.TestCase):
-
+    
+    #testing my repr function
     def test_repr(self):
         loc1 = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc1),"Location('SLO', 35.3, -120.7)")
@@ -11,6 +12,7 @@ class TestLab1(unittest.TestCase):
         loc4 = loc1
         self.assertEqual(repr(loc4),"Location('SLO', 35.3, -120.7)")
     
+    #testing my eq function
     def test_eq(self):
         loc1 = Location("SLO", 35.3, -120.7)
         loc2 = Location("Paris", 48.9, 2.4)
